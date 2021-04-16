c9bfe110a6f42641339720829b877608e3dea0ea
Kristen Bair <kristenmbair@gmail.com>
2021-04-15 18:11:44 -0600

    Create basic component buildout

---
+++ src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import InfoCard from './InfoCard'
+
+const Dashboard = () => {
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <section>
+        <InfoCard />
+        <InfoCard />
+        <InfoCard />
+        <InfoCard />
+      </section>
+    </>
+  )
+}
+
+export default Dashboard
