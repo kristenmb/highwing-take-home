2e431479d429f92bc135328c02b5792da1a65c7d
Kristen Bair <kristenmbair@gmail.com>
2021-04-16 15:01:04 -0600

    Add icons for top of header, adjust header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useQuery } from '@apollo/client'
 import { BROKER_QUERY } from '../queries'
+import arrow from '../Back arrow.svg'
+import bell from '../bell-icon.png'
 import './Header.css'
 
 const Header = () => {
@@ -11,13 +13,19 @@ const Header = () => {
 
   return (
     <header>
+      <div className='top-nav'>
+        <img className='arrow' src={arrow} alt='Back arrow'/>
+        <p className='name'>Elwin Sharvill</p>
+        <p className='name-short'>ES</p>
+        <img className='bell' src={bell} alt='Notification bell'/>
+      </div>
       <h1>{data.mostRecentSnapshot.broker.name}</h1>
       <div className='website'>
-        <p>Website:</p>
+        <p className='label'>Website:</p>
         <p>broker@example.com</p>
       </div>
       <div className='summary'>
-        <p>Summary:</p>
+        <p className='label'>Summary:</p>
         <p>{data.mostRecentSnapshot.broker.description}</p>
       </div>
     </header>
