d2844a6e8db353d99dfe620268dec8fda4cd96d1
Kristen Bair <kristenmbair@gmail.com>
2021-04-15 18:12:22 -0600

    Add components to App to be rendered, adjust filepath for App import in index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
+import React from 'react'
+import Dashboard from './Dashboard'
+import Header from './Header'
+import Nav from './Nav'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h1>APP</h1>
+     <Header />
+     <Nav />
+     <Dashboard />
     </div>
   );
 }
