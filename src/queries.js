6d9f3c78487950f2c1e39f4ac5b056cab7d5a952
Kristen Bair <kristenmbair@gmail.com>
2021-04-15 18:53:20 -0600

    Remove unecessary queries

--- src/queries.js
+++ src/queries.js
@@ -11,44 +11,6 @@ export const BROKER_QUERY = gql`
   }
 `
 
-export const BROKER_SLICE_QUERY = gql`
-  query {
-    mostRecentSnapshot {
-      brokerSlice {
-        industries {
-          premium
-          proportion
-          title
-        }
-        premiumRange {
-          premium
-          proportion
-          title
-        }
-      }
-    }
-  }
-`
-
-export const CARRIER_QUERY = gql`
-  query {
-    mostRecentSnapshot {
-      carrierSlice {
-        industries {
-          premium
-          proportion
-          title
-        }
-        premiumRange {
-          premium
-          proportion
-          title
-        }
-      }
-    }
-  }
-`
-
 export const BROKER_CARRIER_SLICE_QUERY = gql`
   query {
     mostRecentSnapshot {
