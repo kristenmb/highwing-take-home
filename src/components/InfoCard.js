ba9c4422c19d2d1e6012f123e3a8fe372668499c
Kristen Bair <kristenmbair@gmail.com>
2021-04-15 19:24:53 -0600

    Populate chart data set with corresponding premiums and titles

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Doughnut } from 'react-chartjs-2'
 
-const InfoCard = () => {
+const InfoCard = ({ data }) => {
+  const labels = data.map(item => item.title)
+  const dataPoints = data.map(item => item.premium)
+
+  const chartData = {
+    labels,
+    datasets: [{
+      data: dataPoints,
+      backgroundColor: [
+      '#FF6384',
+      '#36A2EB',
+      '#FFCE56',
+      '#b3b3b3'
+      ]
+    }]
+  }
+
   return (
-    <h1>InfoCard</h1>
+    <div style={{height: '45vh', width: '45vw'}}>      
+      <Doughnut
+        data={chartData}
+      />
+    </div>
   )
 }
 
