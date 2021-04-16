ff3a923633b15fe5e0e256d796a7cd2a392aac5c
Kristen Bair <kristenmbair@gmail.com>
2021-04-16 14:21:17 -0600

    Add spacing between info cards

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Nav.css'
 
 const Nav = () => {
   return (
     <div className='pages'>
       <p className='snapshot'>SNAPSHOT</p>
-      <p>TRENDS</p>
+      <p className='trends'>TRENDS</p>
     </div>
   )
 }
