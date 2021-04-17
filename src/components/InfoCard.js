cea42f64d644aa04531591452ab243adeabdf194
Kristen Bair <kristenmbair@gmail.com>
2021-04-16 19:46:55 -0600

    Remove extra spaces, fix indentation

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -3,20 +3,16 @@ import { Doughnut, defaults } from 'react-chartjs-2'
 import { doughnutlabel } from 'chartjs-plugin-doughnutlabel'
 import './InfoCard.css'
 
-
-
 const InfoCard = ({ data }) => {
   const labels = data.map(item => item.title)
   const dataPoints = data.map(item => item.premium.toFixed())
   const percentages = data.map(item => item.proportion.toFixed())
-
 
   const [ percent, setPercent ] = useState(`${percentages[0]}%`)
   const [ premium, setPremium ] = useState(`$${dataPoints[0]}`)
 
   defaults.global.legend.position = 'left'
   defaults.global.legend.labels.usePointStyle = true
- 
 
   const chartData = {
     labels,
@@ -37,36 +33,38 @@ const InfoCard = ({ data }) => {
   }
 
   return (
-    <div style={{height: '25vh', width: '35vw'}}>      
-      <Doughnut
-        data={chartData}
-        options={{
-          cutoutPercentage: 80,
-          legend: {
-            onClick: (e, item) => handleClick(e, item),
-            afterFit: () => this.width = this.width + 50
-          },
-          plugins: {
-            doughnutlabel: {
-              labels: [
-                {
-                  text: `${percent}`,
-                  font: {
-                    family: 'Barlow, sans-serif',
-                    size: '48',
-                  }
-                }],
-                color: 'black',
-                width: 90
+    <>
+      <div className='info-card' style={{height: '25vh', width: '35vw'}}>      
+        <Doughnut
+          data={chartData}
+          options={{
+            cutoutPercentage: 80,
+            legend: {
+              onClick: (e, item) => handleClick(e, item),
+              afterFit: () => this.width = this.width + 50
+            },
+            plugins: {
+              doughnutlabel: {
+                labels: [
+                  {
+                    text: `${percent}`,
+                    font: {
+                      family: 'Barlow, sans-serif',
+                      size: '48',
+                    }
+                  }],
+                  color: 'black',
+                  width: 90
+                }
               }
-            }
-        }}
-      />
+          }}
+        />
+      </div>
       <div className='premium-wrapper'>
         <p className='premium'>{premium}</p>
-        <p>Total Premiums</p>
+        <p className='total'>Total Premiums</p>
       </div>
-    </div>
+    </>
   )
 }
 
